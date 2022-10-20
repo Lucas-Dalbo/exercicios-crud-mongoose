a089e6c887adeb611e6fa1dfdbde21ab008fe055
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-20 15:35:21 -0300

    refactor: compose e connection

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
+const options = {
+  user: process.env.MONGO_INITDB_ROOT_USERNAME, // Usuário do banco de dados.
+  pass: process.env.MONGO_INITDB_ROOT_PASSWORD, // senha do usuário do banco de dados.
+  autoIndex: false, // Não cria index para cada inserção de dado no banco.
+  dbName: process.env.MONGO_INITDB_DATABASE, // Define qual banco de dados vou utilizar.
+};
+
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
-) => mongoose.connect(mongoDatabaseURI);
+    || 'mongodb://localhost:27017/glassesStore?authSource=admin',
+) => mongoose.connect(mongoDatabaseURI, options);
 
 export default connectToDatabase;
