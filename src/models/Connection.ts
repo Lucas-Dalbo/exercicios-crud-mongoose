ece9c617155104a31e541acfb980efa714ae9f39
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-20 16:12:52 -0300

    refactor: Connection - Alternativas ao .env

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 const options = {
-  user: process.env.MONGO_INITDB_ROOT_USERNAME, // Usuário do banco de dados.
-  pass: process.env.MONGO_INITDB_ROOT_PASSWORD, // senha do usuário do banco de dados.
+  user: process.env.MONGO_INITDB_ROOT_USERNAME || 'root', // Usuário do banco de dados.
+  pass: process.env.MONGO_INITDB_ROOT_PASSWORD || '123456', // senha do usuário do banco de dados.
   autoIndex: false, // Não cria index para cada inserção de dado no banco.
-  dbName: process.env.MONGO_INITDB_DATABASE, // Define qual banco de dados vou utilizar.
+  dbName: process.env.MONGO_INITDB_DATABASE || 'glassesStore', // Define qual banco de dados vou utilizar.
 };
 
 const connectToDatabase = (
